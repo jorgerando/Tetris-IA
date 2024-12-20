09f75dd594adb4f01134982c4ba75722fe6f9b9f
Jorge Rando <jorgerando2000@hotmail.com>
2024-12-20 17:04:05 +0000

    Skip legal , home and level screen

--- TetrisEnv.py
+++ TetrisEnv.py
@@ -4,18 +4,30 @@ from nes_py import NESEnv
 rom_path = "./Tetris.nes"
 
 class TetrisEnv(NESEnv):
+
     def __init__(self, rom_file):
+
         super().__init__(rom_path)
 
+    def _did_reset(self):
+        
+         # Skip the legal notice, the home screen and the level selection
+         for _ in range(150):
+          self.ram[0x00C3] = 0x00
+          self.ram[0x00C0] = 0x04
+          self._frame_advance(4)
 
+         return None
 
 env = TetrisEnv(rom_path)
 state = env.reset()
+env._did_reset()
 
-done = False
 while True :
     env.render()
     action = env.action_space.sample()
     state, reward, done, info = env.step(4)
+
+    env.ram[0x00C0] = 0x04
 
 env.close()
